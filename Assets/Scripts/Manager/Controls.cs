3f4dd2526f11895e692454323d3bf40b8d955f4f
Pierre CAMILLI <camillipierre@gmail.com>
2018-01-26 22:09:44 +0100

    Mise en place des scripts.

---
+++ Assets/Scripts/Manager/Controls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : SingletonBehaviour<Controls> {
+
+    [SerializeField]
+    private List<PlayerControls> _players;
+    public PlayerControls Player(int i = 0)
+    {
+        return _players[i];
+    }
+
+    [System.Serializable]
+    public class PlayerControls
+    {
+        [SerializeField]
+        KeyCode _leftKey;
+
+        public float Left
+        {
+            get { return Input.GetKey(_leftKey) ? 1.0f : 0f; }
+        }
+
+        public bool Jump
+        {
+            get { return Input.GetKey(KeyCode.UpArrow); }
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonBehaviour<GameManager> {
+
+    private int _level = 1;
+    public int Level
+    {
+        get { return _level; }
+    }
+
+    [System.Serializable]
+    public class TransfertManager
+    {
+        private float _counter;
+        public float Counter
+        {
+            get { return _counter; }
+            set { _counter = value; }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
